0bef36065c15abaf5335df510ae966e2b5c1d92e
jthor <Thorsen.J@gmail.com>
2018-01-23 21:13:59 -0500

    Template conditional

--- src/components/JokeCounter.vue
+++ src/components/JokeCounter.vue
@@ -2,6 +2,9 @@
   <div class="hello">
     <h1>The joker joked {{ count }} times to the scary thing!</h1>
     <h2><router-link :to="{ name: 'JokeCounter', params: { count: countplusone }}">Joke Again?</router-link></h2>
+
+    <h3 v-if="isCountEnough">Boo! has laughed so hard he vanished!</h3>
+    <h3 v-else>Boo! is still mad!</h3>
   </div>
 </template>
 
@@ -12,6 +15,10 @@ export default {
   computed: {
     countplusone: function () {
       return parseInt(this.count, 10) + 1
+    },
+    isCountEnough: function () {
+      var count = parseInt(this.count, 10)
+      return count >= 100
     }
   }
 }

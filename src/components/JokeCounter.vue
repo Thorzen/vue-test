32e52da573957042f5a73e7c8d8917c0f0f1fcb9
jthor <jthorsen@outlook.com>
2018-01-23 23:42:05 -0500

    Fixes, minor changes, reset now goes through the router to update the URL

--- src/components/JokeCounter.vue
+++ src/components/JokeCounter.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="hello">
     <h1>The hero joked {{ counter }} times to {{ ghostName }}!</h1>
-    <h2><router-link :to="{ name: 'JokeCounter', params: { count: countplusone }}">Joke Again?</router-link></h2>
+    <h2><router-link :to="{ name: 'JokeCounter', params: { count: countPlusOne }}">Joke Again?</router-link></h2>
 
     <h3 v-if="isCountEnough">{{ ghostName }} has laughed so hard he vanished!</h3>
     <h3 v-else>Boo! is still mad!</h3>
 
     Ghost's name: <input v-model="ghostName" />
 
-    <h3 v-on:click="selfDestructClicked">Self Destruct?</h3>
+    <h3 v-on:click="onSelfDestruct">Self Destruct?</h3>
   </div>
 </template>
 
@@ -19,25 +19,25 @@ export default {
   data () {
     return {
       ghostName: 'Boo',
-      counter: 0
+      counter: parseInt(this.count, 10)
     }
   },
   watch: {
-    count(newCount) {
-      this.counter = newCount
+    count (newCount) {
+      this.counter = parseInt(newCount, 10)
     }
   },
   methods: {
-    selfDestructClicked () {
-      this.counter = 0
+    onSelfDestruct () {
+      this.$router.push({name: 'JokeCounter', params: { count: 0 }})
     }
   },
   computed: {
-    countplusone () {
-      return parseInt(this.counter, 10) + 1
+    countPlusOne () {
+      return this.counter + 1
     },
     isCountEnough () {
-      var count = parseInt(this.counter, 10)
+      var count = this.counter
       return count >= 100
     }
   }

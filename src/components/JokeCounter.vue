08ff6fdbab00cd2fe50c2e0f8349f2bab10e97a9
jthor <jthorsen@outlook.com>
2018-01-23 22:02:02 -0500

    Input binding testing

--- src/components/JokeCounter.vue
+++ src/components/JokeCounter.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="hello">
-    <h1>The joker joked {{ count }} times to the scary thing!</h1>
+    <h1>The hero joked {{ count }} times to {{ ghostName }}!</h1>
     <h2><router-link :to="{ name: 'JokeCounter', params: { count: countplusone }}">Joke Again?</router-link></h2>
 
-    <h3 v-if="isCountEnough">Boo! has laughed so hard he vanished!</h3>
+    <h3 v-if="isCountEnough">{{ ghostName }} has laughed so hard he vanished!</h3>
     <h3 v-else>Boo! is still mad!</h3>
+
+    Ghost's name: <input v-model="ghostName" />
   </div>
 </template>
 
@@ -12,6 +14,11 @@
 export default {
   name: 'JokeCounter',
   props: ['count'],
+  data: () => {
+    return {
+      ghostName: 'Boo'
+    }
+  },
   computed: {
     countplusone: function () {
       return parseInt(this.count, 10) + 1

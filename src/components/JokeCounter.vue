4ecbe5fcca0a24e71db1d49756a12d726c9859c5
jthor <Thorsen.J@gmail.com>
2018-01-23 21:01:50 -0500

    Joke Counter

---
+++ src/components/JokeCounter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>The joker joked {{ count }} times to the scary thing!</h1>
+    <h2><router-link :to="{ name: 'JokeCounter', params: { count: countplusone }}">Joke Again?</router-link></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'JokeCounter',
+  props: ['count'],
+  computed: {
+    countplusone: function () {
+      return parseInt(this.count, 10) + 1
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import JokeCounter from '@/components/JokeCounter'
 
 Vue.use(Router)
 
@@ -10,6 +11,12 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/joke/:count',
+      name: 'JokeCounter',
+      component: JokeCounter,
+      props: true
     }
   ]
 })

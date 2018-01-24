130f6909531e47e748d0f9feac52f75df69e4e17
jthor <jthorsen@outlook.com>
2018-01-23 22:50:12 -0500

    Added self destruct button

--- src/components/JokeCounter.vue
+++ src/components/JokeCounter.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="hello">
-    <h1>The hero joked {{ count }} times to {{ ghostName }}!</h1>
+    <h1>The hero joked {{ counter }} times to {{ ghostName }}!</h1>
     <h2><router-link :to="{ name: 'JokeCounter', params: { count: countplusone }}">Joke Again?</router-link></h2>
 
     <h3 v-if="isCountEnough">{{ ghostName }} has laughed so hard he vanished!</h3>
     <h3 v-else>Boo! is still mad!</h3>
 
     Ghost's name: <input v-model="ghostName" />
+
+    <h3 v-on:click="selfDestructClicked">Self Destruct?</h3>
   </div>
 </template>
 
@@ -14,17 +16,28 @@
 export default {
   name: 'JokeCounter',
   props: ['count'],
-  data: () => {
+  data () {
     return {
-      ghostName: 'Boo'
+      ghostName: 'Boo',
+      counter: 0
+    }
+  },
+  watch: {
+    count(newCount) {
+      this.counter = newCount
+    }
+  },
+  methods: {
+    selfDestructClicked () {
+      this.counter = 0
     }
   },
   computed: {
-    countplusone: function () {
-      return parseInt(this.count, 10) + 1
+    countplusone () {
+      return parseInt(this.counter, 10) + 1
     },
-    isCountEnough: function () {
-      var count = parseInt(this.count, 10)
+    isCountEnough () {
+      var count = parseInt(this.counter, 10)
       return count >= 100
     }
   }

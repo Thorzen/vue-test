b1c1efad6460978f13410739fc2f97579bb87f08
jthor <jthorsen@outlook.com>
2018-01-25 22:27:49 -0500

    Some styling

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="hello">
-    <h1>Manage Todos</h1>
-  New Todo: <input class="new-todo-input" ref="newTodoInput" type="text"
-              @keyup.enter="todos.push({name: $event.target.value, done: false, id: ++nextId})">
-    <h2>Incomplete</h2>
-    <ul class="incomplete-todos">
+    <h1 class="header">My Todos</h1>
+    <div class="new-todo"><div class="new-todo-label">New Todo</div><input class="new-todo-input" ref="newTodoInput" type="text"
+              @keyup.enter="todos.push({name: $event.target.value, done: false, id: ++nextId})"></div>
+    <h2 class="incomplete-header">Incomplete</h2>
+    <ul class="incomplete todos">
       <li :key="todo.id" v-for="todo in incomplete">
-        <input type="checkbox" v-on:change="todo.done = true">{{ todo.name }}
+        <input type="checkbox" v-on:change="todo.done = true"><span>{{ todo.name }}</span>
       </li>
     </ul>
-  <h2>Complete</h2>
-    <ul class="complete-todos">
+  <h2 class="complete-header">Complete</h2>
+    <ul class="complete todos">
       <li :key="todo.id" v-for="todo in completed">
-        <input type="checkbox" checked="checked" v-on:change="todo.done = false"> {{ todo.name }}
+        <input type="checkbox" checked="checked" v-on:change="todo.done = false"> <span>{{ todo.name }}</span>
       </li>
     </ul>
   </div>
@@ -22,11 +22,12 @@
 export default {
   name: 'Todo',
   data () {
+    var nextId = 0
     return {
-      nextId: 0,
+      nextId: nextId,
       todos: [{
         name: 'Test This App',
-        id: 0,
+        id: nextId++,
         done: false
       }]
     }
@@ -44,3 +45,54 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.todos
+{
+  list-style-type: none;
+  font-size: 20px;
+}
+
+.new-todo
+{
+  background-color: #ffa4a4;
+  margin: auto;
+  padding: 0 15px 29px 15px;
+  width: 50%;
+}
+
+.new-todo-label
+{
+  vertical-align: middle;
+  color: rgb(161, 96, 96);
+  font-size: 24px;
+}
+
+.new-todo input
+{
+  width: 100%;
+  line-height: 30px;
+  font-size: 30px;
+  border: 1px solid rgb(192, 148, 148);
+}
+
+.todos.complete li span
+{
+    text-decoration: line-through;
+}
+
+.header
+{
+  font-size: 40px;
+  background-color: rgb(255, 228, 228);
+  color: #bd6f6f;
+}
+
+.complete-header, .incomplete-header
+{
+  font-size: 30px;
+  margin: 15px;
+  color: #bd6f6f;
+}
+
+</style>
